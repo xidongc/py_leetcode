ff160a8e3476445a6884eefd005bf7a4c3e32afb
huijiang0102 <mengfei3@illinois.edu>
2018-05-29 19:51:29 +0800

    Create 168_excel_sheet_title.py

---
+++ math/168_excel_sheet_title.py
@@ -0,0 +1,15 @@
+class Solution:
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n == 0:
+            return ""
+        slist = [chr(x) for x in range(ord('A'),ord('Z')+1)]
+        res = ""
+        while n > 0:
+            res += slist[(n-1)%26]
+            n = (n-1)//26
+        return res[::-1]
+        
