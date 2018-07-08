24aae646cc86c820238aac9138dc35dac5166f07
huijiang0102 <mengfei3@illinois.edu>
2018-07-08 19:49:08 +0800

    Create NestedListWeightSum||.py

---
+++ dfs/NestedListWeightSum||.py
@@ -0,0 +1,65 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution(object):
+    def depthSumInverse(self, nestedList):
+        """
+        :type nestedList: List[NestedInteger]
+        :rtype: int
+        """
+        weighted = 0
+        unweighted = 0
+        while nestedList:
+            tempList = []
+            for element in nestedList:
+                if element.isInteger():
+                    unweighted += element.getInteger()
+                else:
+                    tempList += element.getList()
+            weighted += unweighted
+            nestedList = tempList
+        return weighted
+  # Each integer get added one extra time for the mere existence of each one level under it.      
+  # Given the list [[1,1],2,[1,1]], return 8. (four 1's at depth 1, one 2 at depth 2)
+            
+        
