cece7f076c8a5b49de7c1670a6b76fc3fc72dde4
huijiang0102 <mengfei3@illinois.edu>
2018-08-05 21:40:56 +0800

    8/5【67】【85】【389】

--- test.py
+++ test.py
@@ -1,6 +1,30 @@
-zero = 0
-nums = [1,0,1,0,4]
-for i in range(len(nums)):
-    if not nums[i] == 0 and zero <= i:
-        nums[i], nums[zero] = nums[zero], nums[i]
-        zero += 1
+class Solution(object):
+    def maximalRectangle(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+# 暴力
+        if not matrix or not matrix[0]:
+            return 0
+        row = len(matrix)
+        col = len(matrix[0])
+        heights = [0] * col
+        res = 0
+        for i in range(row):
+            for j in range(col):
+                if matrix[i][j] == '0':
+                    heights[j] = 0
+                    continue
+                heights[j] += 1
+                minHeight = heights[j]
+                for k in range(j,-1,-1):
+                    minHeight = min(minHeight,heights[k])
+                    res = max(res, minHeight*(j-k+1))
+        return res
+# DP
+s = Solution()
+print(s.maximalRectangle([
+  ["1","0","1","0","0"],
+  ["1","0","1","1","1"]]))
+
