9768fbdd60406e23bc068b840a2f0e41eee886e3
huijiang0102 <mengfei3@illinois.edu>
2018-08-05 21:45:26 +0800

    8/5【67】【85】【389】

--- array/maximal-rectangle.py
+++ array/maximal-rectangle.py
@@ -66,3 +66,27 @@ heights = [3, 1, 3, 2, 2]
 
 s = Solution()
 print(s.maximalRectangle(input))
+# lmf暴力
+class Solution(object):
+    def maximalRectangle(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix or not matrix[0]:
+            return 0
+        row = len(matrix)
+        col = len(matrix[0])
+        heights = [0] * col
+        res = 0
+        for i in range(row):
+            for j in range(col):
+                if matrix[i][j] == '0':
+                    heights[j] = 0
+                    continue
+                heights[j] += 1
+                minHeight = heights[j]
+                for k in range(j,-1,-1):
+                    minHeight = min(minHeight,heights[k])
+                    res = max(res, minHeight*(j-k+1))
+        return res
