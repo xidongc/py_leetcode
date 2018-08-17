f8ce7810f09139ef10761d2ad839e2844f02e458
huijiang0102 <mengfei3@illinois.edu>
2018-08-17 15:07:41 +0800

    8/17【336】【491】【680】

---
+++ string/680 valid palindrome ||.py
@@ -0,0 +1,18 @@
+# palindrome中间也是palindrome呀
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if not s:
+            return False
+        left,right = 0,len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                # 不要left或者不要right
+                a,b = s[left:right],s[left+1:right+1]
+                return a == a[::-1] or b == b[::-1]
+            left +=1
+            right -= 1
+        return True
