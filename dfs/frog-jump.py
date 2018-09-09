26c953fb9e4ead18fd518090f990e75252973359
Xidong <chenxidong2009@hotmail.com>
2018-09-09 15:36:42 +0800

    clean up test

--- dfs/frog-jump.py
+++ dfs/frog-jump.py
@@ -44,8 +44,33 @@ class Solution(object):
 
 
 # Sol-2:
+# memorize for those dead end, Accepted
+# get sol from https://leetcode.com/problems/frog-jump/discuss/88873/Python-DFS-easy-understanding-using-memo
+# don' t expect myself to write it, can skip it when review
+class Solution(object):
+    def canCross(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: bool
+        """
+        target, stones, memo = stones[-1], set(stones), set()
+        return self.backtrack(stones, 1, 1, target, memo)
+
+    def backtrack(self, stones, pos, jump, target, memo):
+        if (pos, jump) in memo:
+            return False
+        if pos == target:
+            return True
+        if jump <= 0 or pos not in stones:
+            return False
+        for j in (jump-1, jump, jump+1):
+            if self.backtrack(stones, pos+j, j, target, memo):
+                return True
+        memo.add((pos, jump))   # record bad position and jump
+        return False
 
 
+# test
 s = Solution()
 print(s.canCross([0,1,3,5,6,8,12,17]))
 
