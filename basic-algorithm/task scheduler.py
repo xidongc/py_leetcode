7c8171f6239644ab38e386e220594ed51a5f8296
Xidong <chenxidong2009@hotmail.com>
2019-04-01 21:46:58 +0800

    add task scheduler

--- basic-algorithm/task scheduler.py
+++ basic-algorithm/task scheduler.py
@@ -1,1 +1,32 @@
+from queue import PriorityQueue
 
+
+class Solution(object):
+
+    def leastInterval(self, tasks, n: int) -> int:
+
+        if len(tasks) <= 0:
+            return 0
+
+        cycle = 0
+        hashmap = dict()
+        pq = PriorityQueue()
+
+        for task in tasks:
+            hashmap[task] = hashmap.get(task, 0) + 1
+
+        for x in hashmap.values():
+            pq.put(-x)
+
+        while not pq.empty():
+            tmp = list()
+            for _ in range(n + 1):
+                if pq.qsize() > 0:
+                    tmp.append(-pq.get())
+
+            for t in tmp:
+                if t - 1 > 0:
+                    pq.put(-t + 1)
+
+            cycle += len(tmp) if pq.empty() else n + 1
+        return cycle
